596d08220dee80549e365454d76ba9f49e5fc899
Jhonatan de Paula <jhonatandev8@gmail.com>
2024-03-10 17:41:50 -0300

    first commit

---
+++ LHpet/Models/Contexto.cs
@@ -0,0 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace LHpet.Models
+{
+    public class Contexto : DbContext 
+    {
+        public Contexto(DbContextOptions<Contexto>options) : base(options)
+        {
+
+
+        }
+
+        public DbSet<Cliente> Cliente {get; set;}
+
+    }
+ }
+
+
+
